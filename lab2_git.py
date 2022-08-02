3e21f5eeb9d40854f357f5e6b91504c75ca81178
Huzz <msgt.huzz@gmail.com>
2022-08-03 11:53:46 +1200

    Format docstrings

--- lab2_git.py
+++ lab2_git.py
@@ -5,14 +5,14 @@ The code is written for COMPSCI235 Lab 2 Git.
 
 Tasks 1 -- Branching and merging
     - Student A creates an empty public repo on GitHub (Do not include README nor
-    copy right file).
+      copy right file).
     - Student A commits this project to the repo.
     - Student A creates a `requirement.txt` file and commit it.
     - Student A adds Student B as a collaborator.
     - Student B clones the repo to B's local machine.
     - Student B finds a correct `.gitignore` file, and commit it.
     - Both students create and checkout a new branch, A's branch is called "rand",
-    and B's branch is called "coin".
+      and B's branch is called "coin".
     - Student A implements the `get_rand()` function.
     - Student B implements the `toss_coin()` function.
     - For both students, commit your code, and then merge to main.
@@ -28,22 +28,21 @@ Task 2 -- Forking and pulling request
     [New task starts here:]
     - Student A forks the repo.
     - Student A clones the forked repo to A's local machine. (The repo that under A's
-    name.)
+      name.)
     - Student A implements the `toss_coin()` function, and commits to the main branch.
-    (Note: A and B are working on different repositories. A commits to A's forked
-    main.)
-    - Student A send a pull request and merge the code.
+      (Note: A and B are working on different repositories. A commits to A's forked
+      main.)
+    - Student A sends a pull request and merge the code.
 
     You may delete your repo after all tasks have completed.
 
 Questions:
     1. In a scenario, where you work with others as a team. You wrote some code
-    using the lab machine, but the code isn't working. You decide to continue
-    working on the code from home. What's the best way to commit your unfinished code
-    to GitHub without interrupt others? What commands do you need to achieve that?
+       using the lab machine, but the code isn't working. You decide to continue
+       working on the code from home. What's the best way to commit your unfinished code
+       to GitHub without interrupt others? What commands do you need to achieve that?
 
-    2. Explain the difference between merge and fork. Give an example for each use
-    case.
+    2. Explain the difference between merge and fork. Give an example for each use case.
 
 Author: Luke Change (xcha011@aucklanduni.ac.nz)
 Date: 23/07/2022
